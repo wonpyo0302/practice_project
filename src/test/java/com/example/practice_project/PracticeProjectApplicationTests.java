11ccbcbf7caa1987424d08482d88865f1e3b5164
wplee\wonpyo <wplee0102@plateer.com>
2025-03-19 13:10:51 +0900

    frist commit

---
+++ src/test/java/com/example/practice_project/PracticeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practice_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
